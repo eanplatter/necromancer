3f5b002ce7d6b75551e5b3e9103ea8d0ccee1490
eanplatter <ean.platter@gmail.com>
2016-10-09 13:06:12 -0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 import shell from 'shelljs'
 
+const templateName = process.argv[2]
+const word = process.argv[3]
+const newFile = process.argv[4]
+console.log(process.env.PWD)
+
+shell.cp(`~/.spells/${templateName}`, `${process.env.PWD}/${newFile}`)
+
 
