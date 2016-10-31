4f600dced66a60f0d4581dfd2ef6334c7d4c60df
eanplatter <ean.platter@gmail.com>
2016-10-30 20:17:29 -0500

    fix indentations

--- postinstall.js
+++ postinstall.js
@@ -3,18 +3,18 @@ const shell = require('shelljs')
 const fs = require('fs')
 
 fs.access('/Users/eanplatter/.spells', fs.F_OK, function(err) {
-	if (err) {
-		console.log('You are without spells, I will lend you an empty spellbook.')
-		console.log('')
-		console.log('You will want to save your template files in your spellbook by copying them there.')
-		console.log('')
-		console.log('Example: cp myTemplate.html ~/.spells/myTemplate.html')
-		shell.mkdir('~/.spells')
-	} else {
-		console.log('Ah yes, you seem to already have a spellbook')
-		console.log('')
-		console.log('You will want to save your template files in your spellbook by copying them there.')
-		console.log('')
-		console.log('Example: cp myTemplate.html ~/.spells/myTemplate.html')
-	}
+  if (err) {
+    console.log('You are without spells, I will lend you an empty spellbook.')
+    console.log('')
+    console.log('You will want to save your template files in your spellbook by copying them there.')
+    console.log('')
+    console.log('Example: cp myTemplate.html ~/.spells/myTemplate.html')
+    shell.mkdir('~/.spells')
+  } else {
+    console.log('Ah yes, you seem to already have a spellbook')
+    console.log('')
+    console.log('You will want to save your template files in your spellbook by copying them there.')
+    console.log('')
+    console.log('Example: cp myTemplate.html ~/.spells/myTemplate.html')
+  }
 })
